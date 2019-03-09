c15b80ee8438d8881e75bfeb57be695113979d86
chris <christopherchateau@gmail.com>
2019-03-08 21:42:33 -0700

    Update migration file with desired keys for visitors table

--- migrations/20190308211356_initial.js
+++ migrations/20190308211356_initial.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable("visitor", table => {
+      table.increments("id").primary();
+      table.string("ip");
+      table.string("org");
+      table.string("postal_code");
+      table.string("country");
+      table.string("region");
+      table.string("city");
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable("visitors"),
+  ]);
 };
